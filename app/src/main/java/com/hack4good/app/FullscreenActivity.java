1b5913309d505d6e6b3c3fb5b3a9e0a66bd79315
Sasha Renninger <sashafr@sas.upenn.edu>
2014-02-08 12:43:16 -0500

    simple map

--- app/src/main/java/com/hack4good/app/FullscreenActivity.java
+++ app/src/main/java/com/hack4good/app/FullscreenActivity.java
@@ -17,6 +17,7 @@ import android.view.View;
  * @see SystemUiHider
  */
 public class FullscreenActivity extends Activity {
+
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
