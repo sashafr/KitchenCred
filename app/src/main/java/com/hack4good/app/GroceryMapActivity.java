d63f3084a0454aa4a0c9ccd971dd24bc2d17886d
Sasha Renninger <sashafr@sas.upenn.edu>
2014-02-08 17:07:03 -0500

    map elements of grocery map feature

--- app/src/main/java/com/hack4good/app/GroceryMapActivity.java
+++ app/src/main/java/com/hack4good/app/GroceryMapActivity.java
@@ -1,9 +1,322 @@
 package com.hack4good.app;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.location.Criteria;
+import android.location.LocationManager;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v4.app.FragmentActivity;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.Interpolator;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerDragListener;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.GroundOverlay;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by Sasha on 2/8/14.
  */
-public class GroceryMapActivity extends FragmentActivity{
+public class GroceryMapActivity extends FragmentActivity
+        implements
+        OnMarkerClickListener,
+        OnInfoWindowClickListener,
+        OnMarkerDragListener,
+        SeekBar.OnSeekBarChangeListener {
+
+    /** Demonstrates customizing the info window and/or its contents. */
+    class CustomInfoWindowAdapter implements InfoWindowAdapter {
+        private final RadioGroup mOptions;
+
+        // These a both viewgroups containing an ImageView with id "badge" and two TextViews with id
+        // "title" and "snippet".
+        private final View mWindow;
+        private final View mContents;
+
+        CustomInfoWindowAdapter() {
+            mWindow = getLayoutInflater().inflate(R.layout.custom_info_window, null);
+            mContents = getLayoutInflater().inflate(R.layout.custom_info_contents, null);
+            mOptions = (RadioGroup) findViewById(R.id.custom_info_window_options);
+        }
+
+        @Override
+        public View getInfoWindow(Marker marker) {
+            if (mOptions.getCheckedRadioButtonId() != R.id.custom_info_window) {
+                // This means that getInfoContents will be called.
+                return null;
+            }
+            render(marker, mWindow);
+            return mWindow;
+        }
+
+        @Override
+        public View getInfoContents(Marker marker) {
+            if (mOptions.getCheckedRadioButtonId() != R.id.custom_info_contents) {
+                // This means that the default info contents will be used.
+                return null;
+            }
+            render(marker, mContents);
+            return mContents;
+        }
+
+        private void render(Marker marker, View view) {
+
+            String title = marker.getTitle();
+            TextView titleUi = ((TextView) view.findViewById(R.id.title));
+            if (title != null) {
+                // Spannable string allows us to edit the formatting of the text.
+                SpannableString titleText = new SpannableString(title);
+                titleText.setSpan(new ForegroundColorSpan(Color.RED), 0, titleText.length(), 0);
+                titleUi.setText(titleText);
+            } else {
+                titleUi.setText("");
+            }
+
+            String snippet = marker.getSnippet();
+            TextView snippetUi = ((TextView) view.findViewById(R.id.snippet));
+            if (snippet != null && snippet.length() > 12) {
+                SpannableString snippetText = new SpannableString(snippet);
+                snippetText.setSpan(new ForegroundColorSpan(Color.MAGENTA), 0, 10, 0);
+                snippetText.setSpan(new ForegroundColorSpan(Color.BLUE), 12, snippet.length(), 0);
+                snippetUi.setText(snippetText);
+            } else {
+                snippetUi.setText("");
+            }
+
+            //TODO add link to edit info for a location
+        }
+    }
+
+    private GoogleMap mMap;
+
+    private LatLngBounds mBounds;
+
+    private final List<Marker> mMarkerRainbow = new ArrayList<Marker>();
+
+    private TextView mTopText;
+    private SeekBar mRotationBar;
+    private CheckBox mFlatBox;
+
+    private final Random mRandom = new Random();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.marker);
+
+        mTopText = (TextView) findViewById(R.id.top_text);
+
+        mRotationBar = (SeekBar) findViewById(R.id.rotationSeekBar);
+        mRotationBar.setMax(360);
+        mRotationBar.setOnSeekBarChangeListener(this);
+
+        mFlatBox = (CheckBox) findViewById(R.id.flat);
+
+        setUpMapIfNeeded();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap() {
+        // Hide the zoom controls as the button panel will cover it.
+        mMap.getUiSettings().setZoomControlsEnabled(false);
+
+        mBounds = mMap.getProjection().getVisibleRegion().latLngBounds;
+
+        // Add lots of markers to the map.
+        addMarkersToMap(mBounds);
+
+        // Setting an info window adapter allows us to change the both the contents and look of the
+        // info window.
+        mMap.setInfoWindowAdapter(new CustomInfoWindowAdapter());
+
+        // Set listeners for marker events.  See the bottom of this class for their behavior.
+        mMap.setOnMarkerClickListener(this);
+        mMap.setOnInfoWindowClickListener(this);
+        mMap.setOnMarkerDragListener(this);
+
+        mMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
+            @Override
+            public void onCameraChange(CameraPosition cameraPosition) {
+                mBounds = mMap.getProjection().getVisibleRegion().latLngBounds;
+                addMarkersToMap(mBounds);
+            }
+        });
+
+        mMap.setMyLocationEnabled(true);
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String provider = locationManager.getBestProvider(criteria, true);
+        Location myLocation = locationManager.getLastKnownLocation(provider);
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        double latitude = myLocation.getLatitude();
+        double longitude = myLocation.getLongitude();
+        LatLng latLng = new LatLng(latitude, longitude);
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(20));
+        mMap.addMarker(new MarkerOptions().position(latLng)).setTitle("You");
+    }
+
+    private void addMarkersToMap(LatLngBounds bounds) {
+
+/////////    ///////////        ///////////       //         //
+//           //       //        //        //       //      //
+//           //       //        //         //       //   //
+//           //       //        //         //        ////
+//           //       ///       //         //         //
+//           //       //        //       //           //
+//           //       //        //     //             //
+/////////    ///////////        ////////              //
+
+//        // Uses a colored icon.
+//        mBrisbane = mMap.addMarker(new MarkerOptions()
+//                .position(BRISBANE)
+//                .title("Brisbane")
+//                .snippet("Population: 2,074,200")
+//                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+
+//        // Uses a custom icon with the info window popping out of the center of the icon.
+//        mSydney = mMap.addMarker(new MarkerOptions()
+//                .position(SYDNEY)
+//                .title("Sydney")
+//                .snippet("Population: 4,627,300")
+//                .icon(BitmapDescriptorFactory.fromResource(R.drawable.arrow))
+//                .infoWindowAnchor(0.5f, 0.5f));
+
+    }
+
+    private boolean checkReady() {
+        if (mMap == null) {
+            Toast.makeText(this, R.string.map_not_ready, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    /** Called when the Clear button is clicked. */
+    public void onClearMap(View view) {
+        if (!checkReady()) {
+            return;
+        }
+        mMap.clear();
+    }
+
+    /** Called when the Reset button is clicked. */
+    public void onResetMap(View view) {
+        if (!checkReady()) {
+            return;
+        }
+        // Clear the map because we don't want duplicates of the markers.
+        mMap.clear();
+        mBounds = mMap.getProjection().getVisibleRegion().latLngBounds;
+        addMarkersToMap(mBounds);
+    }
+
+    /** Called when the Reset button is clicked. */
+    public void onToggleFlat(View view) {
+        if (!checkReady()) {
+            return;
+        }
+        boolean flat = mFlatBox.isChecked();
+        for (Marker marker : mMarkerRainbow) {
+            marker.setFlat(flat);
+        }
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if (!checkReady()) {
+            return;
+        }
+        float rotation = seekBar.getProgress();
+        for (Marker marker : mMarkerRainbow) {
+            marker.setRotation(rotation);
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        // Do nothing.
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        // Do nothing.
+    }
+
+    //
+    // Marker related listeners.
+    //
+
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+        // We return false to indicate that we have not consumed the event and that we wish
+        // for the default behavior to occur (which is for the camera to move such that the
+        // marker is centered and for the marker's info window to open, if it has one).
+        return false;
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        Toast.makeText(getBaseContext(), "Click Info Window", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+        mTopText.setText("onMarkerDragStart");
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+        mTopText.setText("onMarkerDragEnd");
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+        mTopText.setText("onMarkerDrag.  Current Position: " + marker.getPosition());
+    }
 }
